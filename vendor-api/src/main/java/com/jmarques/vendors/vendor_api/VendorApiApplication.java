3d7c3cceeb0e7ee56edd7aef508d854e7c02bdb6
Joao Marques <johnmarques.br2002@gmail.com>
2024-11-08 07:31:07 -0500

    initial commit

---
+++ vendor-api/src/main/java/com/jmarques/vendors/vendor_api/VendorApiApplication.java
@@ -0,0 +1,13 @@
+package com.jmarques.vendors.vendor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorApiApplication.class, args);
+	}
+
+}
